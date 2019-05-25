516155887ae63990b861a9ebcb343936025fd360
Michael S <mihai.sanfran@gmail.com>
2019-05-25 16:33:07 -0700

    Display image search count using async await

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,31 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    // Callback function invoked in Child component
-    onSearchSubmit(term){
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: 'Client-ID xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-            }
-        });
+    state = {
+        images: []
     }
+
+    // Callback function invoked in Child component
+    // async allows to use async await syntax
+    onSearchSubmit = async (term) => {
+        const response = await axios
+            .get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID ff8bb662d583b86b0fc76d88ab91cef5a46908721a9a5d1c0e43568daf424c3a'
+                }
+            });
+
+        this.setState({
+            images: response.data.results
+        })
+    }
+
     render(){
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
