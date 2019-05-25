079720322e0a7e72d2a2e9698e5418db7f35382c
Michael S <mihai.sanfran@gmail.com>
2019-05-24 22:23:05 -0700

    Create axios GET request using search term

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     // Callback function invoked in Child component
     onSearchSubmit(term){
-        console.log(term)
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+            }
+        });
     }
     render(){
         return (
