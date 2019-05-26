a387ec68d55bcd3fc9abc627f30ac3083a95d3d7
Michael S <mihai.sanfran@gmail.com>
2019-05-25 18:46:38 -0700

    Render images using ImageList component

---
+++ src/components/ImageList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ImageList = props => {
+    // Key is assigned to root element that is rendered
+    const images = props.images.map(({ description, id, urls}) => {
+        return (
+            <div key={id}>
+                <img alt={description} src={urls.regular} />
+            </div>
+        );
+    });
+
+    return <div>{images}</div>;
+};
+
+export default ImageList;
